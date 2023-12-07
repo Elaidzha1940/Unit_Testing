ede9a1ca2db7a47409c86c4d96c7737c7f4e5916
Elaidzha1940 <eli080293@gmail.com>
2023-12-07 11:56:07 +0300

    Unit_Testing

---
+++ Unit_Testing/Unit_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_TestingApp.swift
+//  Unit_Testing
+//
+//  Created by Elaidzha Shchukin on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Unit_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
