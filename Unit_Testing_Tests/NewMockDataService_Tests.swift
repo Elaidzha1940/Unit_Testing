6d62c6756ece4aaa3978fb669a3e4f67aba502b8
Elaidzha1940 <eli080293@gmail.com>
2023-12-08 20:05:43 +0300

    Update NewMockDataService_Tests

--- Unit_Testing_Tests/NewMockDataService_Tests.swift
+++ Unit_Testing_Tests/NewMockDataService_Tests.swift
@@ -12,15 +12,18 @@ import Combine
 @testable import Unit_Testing
 
 final class NewMockDataService_Tests: XCTestCase {
-
+    
+    var cancellables = Set<AnyCancellable>()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        cancellables.removeAll()
     }
-
+    
     func test_NewMockDataService_init_doesSetValuesCorrectly() {
         // Given
         let items: [String]? = nil
@@ -36,6 +39,75 @@ final class NewMockDataService_Tests: XCTestCase {
         XCTAssertFalse(dataSevice.items.isEmpty)
         XCTAssertTrue(dataSevice2.items.isEmpty)
         XCTAssertEqual(dataSevice3.items.count, items3?.count)
+    }
+    
+    func test_NewMockDataService_donwloadItemsWithEscaping_doesReturnValues() {
+        // Given
+        let dataSevice = NewMockDataService(items: nil)
         
+        // When
+        var items: [String] = []
+        let expectation = XCTestExpectation()
+        
+        dataSevice.donwloadItemsWithEscaping { returnedItems in
+            items  = returnedItems
+            expectation.fulfill()
+        }
+        
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(items.count, dataSevice.items.count)
+    }
+    
+    func test_NewMockDataService_donwloadItemsWithCombine_doesReturnValues() {
+        // Given
+        let dataSevice = NewMockDataService(items: nil)
+        
+        // When
+        var items: [String] = []
+        let expectation = XCTestExpectation()
+        
+        dataSevice.donwloadItemsWithCombine()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    expectation.fulfill()
+                case .failure:
+                    XCTFail()
+                }
+            } receiveValue: { returnedItems in
+                items  = returnedItems
+            }
+            .store(in: &cancellables)
+        
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(items.count, dataSevice.items.count)
+    }
+    
+    func test_NewMockDataService_donwloadItemsWithCombine_doesFail() {
+        // Given
+        let dataSevice = NewMockDataService(items: [])
+        
+        // When
+        var items: [String] = []
+        let expectation = XCTestExpectation()
+        
+        dataSevice.donwloadItemsWithCombine()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    XCTFail()
+                case .failure:
+                    expectation.fulfill()
+                }
+            } receiveValue: { returnedItems in
+                items  = returnedItems
+            }
+            .store(in: &cancellables)
+        
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(items.count, dataSevice.items.count)
     }
 }
